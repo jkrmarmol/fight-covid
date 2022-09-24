7f9fc7169717e3095b9f2dfcad087c78531d737a
Kurt Russelle Marmol <103259295+jkrmarmol@users.noreply.github.com>
2022-09-24 20:57:16 +0800

    remove body variable

--- src/Features/CovidCaseSlices.js
+++ src/Features/CovidCaseSlices.js
@@ -1,11 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
-const body = {
-  operationName: null,
-  variables: {},
-  query: "{\n  covidCaseStatus: covid_case_status {\n    active\n    all\n    asymptomatic\n    critical\n    died\n    mild\n    repatriate\n    recovered\n    severe\n    __typename\n  }\n  covidCaseLastUpdated: covid_case_last_updated {\n    updatedAt\n    __typename\n  }\n}\n"
-}
-
 export const getCovidData = createAsyncThunk(
   'covid/getCovidData',
   async (arg, thunkAPI) => {
